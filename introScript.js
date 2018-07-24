b617228fa4bc1061c4e6028784ed2254ec2748eb
storycoding <youknownuno@gmail.com>
2018-07-24 17:51:08 +0100

    created a basic script for voice calls

---
+++ introScript.js
@@ -0,0 +1,24 @@
+[
+    {
+        "action": "talk",
+        "text": "Hello, Thomas. Do you know who this is?",
+        "voiceName": "Amy",
+        "bargeIn": false
+    },
+    {
+        "action": "talk",
+        "text": "Press hash if you think it's Nuno",
+        "voiceName": "Amy",
+        "bargeIn": true
+    },
+    {
+        "action": "input",
+        "submitOnHash": true,
+        "timeOut": 10
+    },
+    {
+        "action": "talk",
+        "text": "I've been looking for you, Thomas. I don't know if you're ready to see what I want to show you. But unfortunately, you and I are running out of time. They are coming for you, Thomas. And I don't know what they're going to do. I can guide you, but you must do exactly as I say.",
+        "voiceName": "Amy"
+    }
+]
